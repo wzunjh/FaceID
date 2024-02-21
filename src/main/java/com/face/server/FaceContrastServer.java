876586f7a96dd5c29f73fa3fec6f03d4aed6284d
njh <wzunjh@qq.com>
2024-02-21 21:20:23 +0800

    人脸识别处理API接入完成

--- src/main/java/com/face/server/FaceContrastServer.java
+++ src/main/java/com/face/server/FaceContrastServer.java
@@ -1,51 +1,52 @@
 package com.face.server;
 
 import com.face.bean.result.FaceResult;
-import com.tencentcloudapi.common.Credential;
-import com.tencentcloudapi.common.exception.TencentCloudSDKException;
-import com.tencentcloudapi.common.profile.ClientProfile;
-import com.tencentcloudapi.common.profile.HttpProfile;
-import com.tencentcloudapi.iai.v20180301.IaiClient;
-import com.tencentcloudapi.iai.v20180301.models.CompareFaceRequest;
-import com.tencentcloudapi.iai.v20180301.models.CompareFaceResponse;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
+import org.springframework.web.client.RestTemplate;
+import java.util.HashMap;
+import java.util.Map;
 /**
- * <p>调用腾讯接口</p>
+ * <p>调用py接口</p>
  **/
 @Component
 @Data
 @Slf4j
 public class FaceContrastServer {
 
-    @Value("${tencentcloudapi.secretId}")
-    private String secretId;
-    @Value("${tencentcloudapi.secretKey}")
-    private String secretKey;
-    @Value("${tencentcloudapi.endpoint}")
-    private String endpoint;
-    @Value("${tencentcloudapi.region}")
-    private String region;
+    @Value("${python.service.url}")
+    private String pythonServiceUrl;
 
-    public FaceResult faceContrast(String imageA, String imageB){
+    public FaceResult faceContrast(String imageA, String imageB) {
         FaceResult faceResult = new FaceResult();
-        try{
-            Credential cred = new Credential(secretId, secretKey);
-            HttpProfile httpProfile = new HttpProfile();
-            httpProfile.setEndpoint(endpoint);
-            ClientProfile clientProfile = new ClientProfile();
-            clientProfile.setHttpProfile(httpProfile);
-            IaiClient client = new IaiClient(cred, region, clientProfile);
-            CompareFaceRequest req = new CompareFaceRequest();
-            req.setImageA(imageA);
-            req.setImageB(imageB);
-            CompareFaceResponse resp = client.CompareFace(req);
-            faceResult.setScore(resp.getScore());
-            faceResult.setCode(FaceResult.SUCCESS_CODE);
-        } catch (TencentCloudSDKException e) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            Map<String, String> map = new HashMap<>();
+            map.put("imageA", imageA);
+            map.put("imageB", imageB);
+
+            HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
+
+            ResponseEntity<Map> response = restTemplate.postForEntity(pythonServiceUrl + "/compare_face", request, Map.class);
+            Map<String, Integer> responseBody = response.getBody();
+
+            if (responseBody != null && responseBody.containsKey("similarity_score")) {
+                faceResult.setScore(Float.valueOf(responseBody.get("similarity_score")));
+                faceResult.setCode(FaceResult.SUCCESS_CODE);
+            } else {
+                faceResult.setCode(FaceResult.FACE_ERROR);
+                faceResult.setMsg("No similarity score found in the response");
+            }
+        } catch (Exception e) {
             faceResult.setCode(FaceResult.FACE_ERROR);
             faceResult.setMsg(e.getMessage());
         }
