4dad4b5e3e234aa3e3d7f6b21466e863418d005f
njh <wzunjh@qq.com>
2024-04-26 01:20:47 +0800

    API日志功能完成

--- src/main/java/com/face/controller/FaceVefLogController.java
+++ src/main/java/com/face/controller/FaceVefLogController.java
@@ -4,17 +4,16 @@ import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.face.bean.ApiLog;
 import com.face.bean.FaceVefLog;
 import com.face.bean.result.FaceResult;
+import com.face.service.ApiLogService;
 import com.face.service.FaceVefLogService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -25,6 +24,10 @@ public class FaceVefLogController {
 
     @Autowired
     FaceVefLogService faceVefLogService;
+
+    @Autowired
+    ApiLogService apiLogService;
+
 
     @GetMapping("/list")
     @ApiOperation(value = "日志接口",notes = "查询所有验证人脸日志信息")
@@ -43,5 +46,32 @@ public class FaceVefLogController {
                 .orderByDesc("vef_time"));
         return FaceResult.success(page);
     }
+
+    @GetMapping("/{fid}")
+    @ApiOperation(value = "查询Api日志信息", notes = "根据id查询")
+    public FaceResult info(
+            @PathVariable Integer fid,
+            @RequestParam Integer current,
+            @RequestParam Integer size,
+            @RequestParam(required = false) String startTime,
+            @RequestParam(required = false) String endTime
+    ) {
+        IPage<ApiLog> page;
+        if (fid.equals(1)) {
+            // fid为1时，查询所有日志信息
+            page = apiLogService.page(new Page<>(current, size), new QueryWrapper<ApiLog>()
+                    .between(StrUtil.isNotBlank(startTime) && StrUtil.isNotBlank(endTime), "api_time", startTime, endTime)
+                    .orderByDesc("api_time"));
+        } else {
+            // fid不为1时，查询fid为传入值的日志信息
+            page = apiLogService.page(new Page<>(current, size), new QueryWrapper<ApiLog>()
+                    .eq("fid", fid)
+                    .between(StrUtil.isNotBlank(startTime) && StrUtil.isNotBlank(endTime), "api_time", startTime, endTime)
+                    .orderByDesc("api_time"));
+        }
+        return FaceResult.success(page);
+    }
+
+
 
 }
