70ef97a21ed169776613d14b4faba3fecb62d493
njh <wzunjh@qq.com>
2024-04-26 15:34:25 +0800

    手机号修改功能完成

--- face-ui/src/components/phoneVef/index.vue
+++ face-ui/src/components/phoneVef/index.vue
@@ -9,12 +9,13 @@
           <el-input v-model="phoneAuthForm.code" clearable></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button @click="sendCode" :disabled="countdown > 0">发送验证码{{countdown ? `(${countdown}s)` : ''}}</el-button>
+          <el-button @click="sendCode('phone')" :disabled="phoneCountdown > 0">发送验证码{{phoneCountdown ? `(${phoneCountdown}s)` : ''}}</el-button>
           <el-button type="primary" @click="submitPhoneForm('phoneForm')">提交核验</el-button>
         </el-form-item>
       </el-form>
     </el-card>
-    <div v-else class="success-message">
+
+    <el-card v-else-if="!modifyPhoneVisible" header="手机号绑定成功" class="mb-4">
       <el-alert title="手机号绑定成功" type="success" center show-icon />
       <el-descriptions title="您的绑定信息">
         <el-descriptions-item label="用户ID">{{ phoneData.fid }}</el-descriptions-item>
@@ -23,7 +24,40 @@
           <el-tag size="small">已绑定</el-tag>
         </el-descriptions-item>
       </el-descriptions>
-    </div>
+      <div class="d-flex justify-content-center">
+        <el-button type="primary" @click="showModifyPhoneForm">修改手机号</el-button>
+      </div>
+    </el-card>
+
+    <el-card v-else-if="modifyPhoneStep === 1" header="修改手机号绑定">
+      <el-form ref="modifyPhoneForm" :model="modifyPhoneForm" label-width="120px">
+        <el-form-item label="原手机号码" prop="oldPhone">
+          <el-input v-model="modifyPhoneForm.oldPhone" clearable disabled></el-input>
+        </el-form-item>
+        <el-form-item label="验证码" prop="oldCode">
+          <el-input v-model="modifyPhoneForm.oldCode" clearable></el-input>
+          <el-button @click="sendCode('old')" :disabled="oldPhoneCountdown > 0">发送验证码{{oldPhoneCountdown ? `(${oldPhoneCountdown}s)` : ''}}</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitOldPhoneForm('modifyPhoneForm')">提交验证</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+
+    <el-card v-else-if="modifyPhoneStep === 2" header="修改手机号绑定">
+      <el-form ref="modifyPhoneForm" :model="modifyPhoneForm" label-width="120px">
+        <el-form-item label="新手机号码" prop="newPhone">
+          <el-input v-model="modifyPhoneForm.newPhone" clearable></el-input>
+        </el-form-item>
+        <el-form-item label="验证码" prop="newCode">
+          <el-input v-model="modifyPhoneForm.newCode" clearable></el-input>
+          <el-button @click="sendCode('new')" :disabled="newPhoneCountdown > 0">发送验证码{{newPhoneCountdown ? `(${newPhoneCountdown}s)` : ''}}</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitNewPhoneForm('modifyPhoneForm')">提交修改</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -36,9 +70,19 @@ export default {
         code: '',
         fid: ''
       },
+      modifyPhoneForm: {
+        oldPhone: '',
+        oldCode: '',
+        newPhone: '',
+        newCode: ''
+      },
       phoneBound: false,
       phoneData: { fid: '', phone: '' },
-      countdown: 0
+      phoneCountdown: 0,
+      oldPhoneCountdown: 0,
+      newPhoneCountdown: 0,
+      modifyPhoneVisible: false,
+      modifyPhoneStep: 1
     };
   },
   mounted() {
@@ -52,20 +96,60 @@ export default {
           this.phoneBound = true;
           this.phoneData.phone = response.data.phone;
           this.phoneData.fid = response.data.fid;
+          this.modifyPhoneForm.oldPhone = response.data.phone;
         }
       }).catch(error => {
         console.error('Error checking phone binding:', error);
       });
     },
-    sendCode() {
-      if (this.countdown > 0) return;
-      this.countdown = 60;
+    sendCode(type) {
+      let countdown = 0;
+      switch (type) {
+        case 'phone':
+          countdown = this.phoneCountdown;
+          break;
+        case 'old':
+          countdown = this.oldPhoneCountdown;
+          break;
+        case 'new':
+          countdown = this.newPhoneCountdown;
+          break;
+      }
+      if (countdown > 0) return;
+      countdown = 60;
       let interval = setInterval(() => {
-        this.countdown--;
-        if (this.countdown === 0) clearInterval(interval);
+        countdown--;
+        if (countdown === 0) clearInterval(interval);
+        switch (type) {
+          case 'phone':
+            this.phoneCountdown = countdown;
+            break;
+          case 'old':
+            this.oldPhoneCountdown = countdown;
+            break;
+          case 'new':
+            this.newPhoneCountdown = countdown;
+            break;
+        }
       }, 1000);
 
-      this.$http.get('/face/Sms', { params: { phone: this.phoneAuthForm.phone } })
+      let phone = '';
+      switch (type) {
+        case 'phone':
+          phone = this.phoneAuthForm.phone;
+          break;
+        case 'old':
+          phone = this.modifyPhoneForm.oldPhone;
+          break;
+        case 'new':
+          if (!/^1\d{10}$/.test(this.modifyPhoneForm.newPhone)) {
+            alert('非法手机号');
+            return;
+          }
+          phone = this.modifyPhoneForm.newPhone;
+          break;
+      }
+      this.$http.get('/face/Sms', { params: { phone: phone } })
           .then(response => {
             alert(response.data.msg);
           }).catch(error => {
@@ -95,6 +179,59 @@ export default {
           return false;
         }
       });
+    },
+    showModifyPhoneForm() {
+      this.modifyPhoneVisible = true;
+      this.modifyPhoneStep = 1;
+    },
+    submitOldPhoneForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.$http.get('/face/SmsVef', {
+            params: {
+              fid: this.phoneAuthForm.fid,
+              phone: this.modifyPhoneForm.oldPhone,
+              code: this.modifyPhoneForm.oldCode
+            }
+          }).then(response => {
+            if (response.data.code === 200) {
+              this.modifyPhoneStep = 2;
+            } else {
+              alert(response.data.msg);
+            }
+          }).catch(error => {
+            console.error('Old phone verification failed:', error);
+          });
+        } else {
+          return false;
+        }
+      });
+    },
+    submitNewPhoneForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.$http.get('/face/SmsVef', {
+            params: {
+              fid: this.phoneAuthForm.fid,
+              phone: this.modifyPhoneForm.newPhone,
+              code: this.modifyPhoneForm.newCode
+            }
+          }).then(response => {
+            if (response.data.code === 200) {
+              this.phoneBound = true;
+              this.checkPhoneBinding();
+              this.modifyPhoneVisible = false;
+              this.modifyPhoneStep = 1;
+            } else {
+              alert(response.data.msg);
+            }
+          }).catch(error => {
+            console.error('New phone verification failed:', error);
+          });
+        } else {
+          return false;
+        }
+      });
     }
   }
 };
@@ -106,6 +243,18 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100vh;
+}
+
+.mb-4 {
+  margin-bottom: 1.5rem;
+}
+
+.d-flex {
+  display: flex;
+}
+
+.justify-content-center {
+  justify-content: center;
 }
 
 .success-message {
