54412fa53c95888a5642c279c37856f85e12758e
njh <wzunjh@qq.com>
2024-04-27 01:47:29 +0800

    OTP动态口令成功

--- face-ui/src/components/otp/index.vue
+++ face-ui/src/components/otp/index.vue
@@ -2,19 +2,27 @@
   <div id="app">
     <h1>OTP Generator</h1>
     <p v-if="error">{{ error }}</p>
-    <p v-else>
-      OTP: {{ otp }}
-      <br>
-      Expiration: {{ expirationSeconds }} seconds
-    </p>
+    <div v-else class="otp-container">
+      <div class="otp-digits">
+        <div class="otp-digit" v-for="digit in otp.split('')" :key="digit">{{ digit }}</div>
+      </div>
+      <div class="countdown">
+        <el-progress :percentage="(expirationSeconds / 30) * 100" :show-text="false" :stroke-width="20" />
+        <div class="countdown-text">{{ expirationSeconds }} seconds</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import { Progress } from 'element-ui';
 
 export default {
   name: 'OtpPage',
+  components: {
+    'el-progress': Progress
+  },
   data() {
     return {
       otp: '',
@@ -58,5 +66,41 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.otp-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 40px;
+}
+
+.otp-digits {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+.otp-digit {
+  font-size: 36px;
+  font-weight: bold;
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin: 0 5px;
+}
+
+.countdown {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.countdown-text {
+  margin-top: 10px;
+  font-size: 18px;
+  font-weight: bold;
 }
 </style>
