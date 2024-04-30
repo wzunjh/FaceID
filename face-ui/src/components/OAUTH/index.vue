dc5693666756124209bfcd9e40f268b4b1a20525
njh <wzunjh@qq.com>
2024-04-30 23:29:58 +0800

    授权第三方登录实现

--- face-ui/src/components/OAUTH/index.vue
+++ face-ui/src/components/OAUTH/index.vue
@@ -12,8 +12,8 @@
             </el-form-item>
             <el-form-item>
               <el-button icon="el-icon-search" @click="getAppList">搜索</el-button>
-              <el-button type="primary" @click="openOtpOauthPage">统一身份信息授权登录中心</el-button>
-              <el-button type="primary" @click="openOtpYyPage">第三方网站授权登录演示</el-button>
+              <el-button v-if="isUserAuthenticated" type="primary" @click="openOtpOauthPage">统一身份信息授权登录中心</el-button>
+              <el-button v-if="isUserAuthenticated" type="primary" @click="openOtpYyPage">第三方网站授权登录演示</el-button>
             </el-form-item>
           </el-form>
         </el-card>
@@ -24,7 +24,7 @@
       <el-col :span="24">
         <el-card>
           <el-header style="background-color: #ffffff">
-            <el-button @click="handleSave()" icon="el-icon-circle-plus-outline">添加应用</el-button>
+            <el-button v-if="isUserAuthenticated" @click="handleSave()" icon="el-icon-circle-plus-outline">添加应用</el-button>
           </el-header>
           <el-table :data="tableData" style="width: 100%">
             <el-table-column prop="clientId" label="客户端ID" sortable></el-table-column>
@@ -34,8 +34,8 @@
             <el-table-column prop="fid" label="所属用户ID" sortable></el-table-column>
             <el-table-column label="操作" width="230">
               <template v-slot:default="scope">
-                <el-button icon="el-icon-edit" size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                <el-popconfirm :title="'确认删除' + scope.row.name + '应用吗?'" @confirm="handleDelete(scope.$index, scope.row)">
+                <el-button v-if="isUserAuthenticated" icon="el-icon-edit" size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                <el-popconfirm v-if="isUserAuthenticated" :title="'确认删除' + scope.row.name + '应用吗?'" @confirm="handleDelete(scope.$index, scope.row)">
                   <el-button style="margin-left: 20px" icon="el-icon-delete" size="mini" type="danger" slot="reference">删除</el-button>
                 </el-popconfirm>
               </template>
@@ -67,8 +67,8 @@
           </el-form-item>
         </el-form-group>
         <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">立即保存</el-button>
-          <el-button @click="resetForm('ruleForm')">重置</el-button>
+          <el-button v-if="isUserAuthenticated" type="primary" @click="submitForm('ruleForm')">立即保存</el-button>
+          <el-button v-if="isUserAuthenticated" @click="resetForm('ruleForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </el-dialog>
@@ -118,7 +118,8 @@ export default {
           { required: true, message: '请输入所属用户ID', trigger: 'blur' }
         ]
       },
-      currentUserFid: null
+      currentUserFid: null,
+      isUserAuthenticated: false
     }
   },
   computed: {
@@ -128,9 +129,21 @@ export default {
   },
   created() {
     this.currentUserFid = parseInt(localStorage.getItem("user_id"));
-    this.getAppList();
+    this.checkUserAuthentication();
   },
   methods: {
+    checkUserAuthentication() {
+      this.$http.get('/face/orAuth/' + this.currentUserFid).then(res => {
+        if (res.data.code === 200) {
+          this.isUserAuthenticated = true;
+          this.getAppList();
+        } else {
+          this.isUserAuthenticated = false;
+          res.data.msg("未认证用户,无权进行此操作")
+          this.$message.warning(res.data.msg);
+        }
+      });
+    },
     getAppList() {
       let params = {
         current: this.current,
@@ -163,57 +176,73 @@ export default {
       this.resetForm('ruleForm')
     },
     handleSave() {
-      this.addAndUpdateState = true
-      this.saveAndUpdateDialog = true
-      this.saveAndUpdateTitle = '添加应用'
+      // 先检查用户认证状态
+      this.checkUserAuthentication();
+      if (this.isUserAuthenticated) {
+        this.addAndUpdateState = true
+        this.saveAndUpdateDialog = true
+        this.saveAndUpdateTitle = '添加应用'
+      }
     },
     handleEdit(index, row) {
-      this.addAndUpdateState = false
-      this.saveAndUpdateDialog = true
-      this.saveAndUpdateTitle = '修改应用'
-      this.appObj = { ...row }
+      // 先检查用户认证状态
+      this.checkUserAuthentication();
+      if (this.isUserAuthenticated) {
+        this.addAndUpdateState = false
+        this.saveAndUpdateDialog = true
+        this.saveAndUpdateTitle = '修改应用'
+        this.appObj = { ...row }
+      }
     },
     handleDelete(index, row) {
-      this.$http.get("/oau/Delete?clientId=" + row.clientId).then(res => {
-        this.getAppList();
-        this.$message.success(res.data.msg)
-      })
+      // 先检查用户认证状态
+      this.checkUserAuthentication();
+      if (this.isUserAuthenticated) {
+        this.$http.get("/oau/Delete?clientId=" + row.clientId).then(res => {
+          this.getAppList();
+          this.$message.success(res.data.msg)
+        })
+      }
     },
     submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          if (!this.isSuperUser) {
-            this.appObj.fid = this.currentUserFid;
-          }
-          if (this.addAndUpdateState) {
-            // 添加应用
-            this.$http.post("/oau/save", this.appObj).then(res => {
-              this.$message({
-                showClose: true,
-                message: res.data.msg,
-                type: 'success',
-                duration: 3000
+      // 先检查用户认证状态
+      this.checkUserAuthentication();
+      if (this.isUserAuthenticated) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            if (!this.isSuperUser) {
+              this.appObj.fid = this.currentUserFid;
+            }
+            if (this.addAndUpdateState) {
+              // 添加应用
+              this.$http.post("/oau/save", this.appObj).then(res => {
+                this.$message({
+                  showClose: true,
+                  message: res.data.msg,
+                  type: 'success',
+                  duration: 3000
+                })
+                this.getAppList()
+                this.saveAndUpdateDialog = false
               })
-              this.getAppList()
-              this.saveAndUpdateDialog = false
-            })
+            } else {
+              // 修改应用
+              this.$http.post("/oau/update", this.appObj).then(res => {
+                this.$message({
+                  showClose: true,
+                  message: res.data.msg,
+                  type: 'success',
+                  duration: 3000
+                })
+                this.getAppList()
+                this.saveAndUpdateDialog = false
+              })
+            }
           } else {
-            // 修改应用
-            this.$http.post("/oau/update", this.appObj).then(res => {
-              this.$message({
-                showClose: true,
-                message: res.data.msg,
-                type: 'success',
-                duration: 3000
-              })
-              this.getAppList()
-              this.saveAndUpdateDialog = false
-            })
+            return false;
           }
-        } else {
-          return false;
-        }
-      });
+        });
+      }
     },
     resetForm(formName) {
       this.$refs[formName].resetFields();
