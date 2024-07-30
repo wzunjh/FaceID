72752552cf41315719ce19d2c3961b2e1c2f6134
njh <wzunjh@qq.com>
2024-07-30 09:38:37 +0800

    EasyExcel与Minio配合的生成与下载功能

--- src/main/java/com/face/config/MinioConfig.java
+++ src/main/java/com/face/config/MinioConfig.java
@@ -1,18 +1,29 @@
 package com.face.config;
 
 import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioConfig {
 
+    @Value("${minio.service.endpoint}")
+    private String minioServiceEndpoint;
+
+    @Value("${minio.service.username}")
+    private String minioServiceUsername;
+
+    @Value("${minio.service.password}")
+    private String minioServicePassword;
+
     @Bean
     public MinioClient minioClient(){
 
+
         return MinioClient.builder()
-                .endpoint("http://127.0.0.1:9000")
-                .credentials("username","password")
+                .endpoint(minioServiceEndpoint)
+                .credentials(minioServiceUsername,minioServicePassword)
                 .build();
 
     }
